e179ab313cd6f4d62a0b8ce95a57854b0fd1fa6a
Rupi Sadhra <varinderjitkaur@developersinstitute.co.nz>
2021-04-28 12:36:41 +1200

    Season Display component created

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const getSeason=(lat,month)=>
+{
+    if(month>2 && month<9)
+    {
+        return lat>0?'summer':'winter';
+    }
+    else
+    {
+        return lat>0?'winter':'summer';
+    }
+}
+
+const SeasonDisplay=(props)=>{
+    const season=getSeason(props.lat, new Date().getMonth())
+    return <div className='season-container'>
+        <h1>{season}</h1>
+    </div>
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state={lat:null,errorMessage:''};
+    }
+
+    render()
+    {
+       if(!this.state.lat && !this.state.errorMessage)
+       {
+        return <div>Loading</div>
+       }
+       if(!this.state.lat && this.state.errorMessage)
+       {
+        return <div>Error Message:{this.state.errorMessage}</div>
+       }
+    
+        return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    componentDidMount()
+    {
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                console.log(position);
+                this.setState({lat:position.coords.latitude});
+            },
+            (error)=>{
+                console.log(error.message);
+               this.setState({errorMessage:error.message})
+            });
+    }
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
